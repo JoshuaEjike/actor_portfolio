0f80d58196ec0bc0e91e3c87503a1cb2343ef4e6
Joshua4221 <joshuaejike4221@gmail.com>
2026-02-10 21:35:10 +0100

    fixed refresh token issues

--- src/utils/cookies.rs
+++ src/utils/cookies.rs
@@ -2,19 +2,21 @@ use tower_cookies::cookie::{Cookie, SameSite, time::Duration};
 
 pub fn set_refresh_cookie(token: String) -> Cookie<'static> {
     Cookie::build(("refresh_token", token))
-        .secure(false) // true in production (HTTPS)
+        .secure(true) // true in production (HTTPS)
         .http_only(true)
-        .same_site(SameSite::Lax)
+        .same_site(SameSite::None)
+        .domain("actor-portfolio.onrender.com")
         .path("/")
-        .max_age(Duration::days(30))
+        .max_age(Duration::days(1))
         .build()
 }
 
 pub fn clear_refresh_cookies() -> Cookie<'static> {
     Cookie::build(("refresh_token", ""))
-        .secure(false) // true in production (HTTPS)
+        .secure(true) // true in production (HTTPS)
         .http_only(true)
-        .same_site(SameSite::Lax)
+        .same_site(SameSite::None)
+        .domain("actor-portfolio.onrender.com")
         .path("/")
         .max_age(Duration::days(30))
         .build()
